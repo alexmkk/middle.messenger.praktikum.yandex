aa45d76cd39fc2dce09e05b86d28ff3da7ee2406
Alexander Mikolyuk <asmkk@mail.ru>
2022-10-09 12:26:59 +0300

    Sprint - 4 fix audit

--- src/utils/Block.test.ts
+++ src/utils/Block.test.ts
@@ -18,10 +18,22 @@ const { default: Block } = proxyquire("./Block", {
 
 describe("Block", () => {
   class ComponentMock extends Block {}
+  const block = new ComponentMock();
 
   it("should fire init event on initialization", () => {
-    new ComponentMock();
-
     expect(eventBusMock.emit.calledWith("init")).to.eq(true);
+  });
+
+  it("block have id", () => {
+    expect(block.id).to.be.a("string");
+  });
+
+  it("setProps() works correctly", () => {
+    block.setProps({ text: "text" });
+    expect(block.props).to.have.property("text");
+  });
+
+  it("should not return null for children", () => {
+    expect(block.children).to.be.not.null;
   });
 });
